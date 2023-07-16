c0d01a80252582c675a0ebcd5768fdb0dc9dd7ea
pingu <nor@acorneroftheweb.com>
2023-07-16 18:20:38 +0200

    Fixed so normal audio also works :nervous_laughter:

--- configuration.nix
+++ configuration.nix
@@ -128,8 +128,8 @@
         ["bluez5.enable-sbc-xq"] = true,
         ["bluez5.enable-msbc"] = true,
         ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        ["bluez5.codecs"] = "[ sbc sbc_xq aac ldac aptx aptx_hd aptx_ll aptx_ll_duplex ]"
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]",
+        ["bluez5.codecs"] = "[ sbc sbc_xq aac ldac aptx aptx_hd aptx_ll aptx_ll_duplex faststream faststream_duplex]"
       }
     '';
   };
