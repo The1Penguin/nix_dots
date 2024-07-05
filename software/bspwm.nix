b50e1de68e3942f037ecc0e84995ec316862957c
pingu <nor@acorneroftheweb.com>
2024-07-05 15:26:44 +0200

    Now fully configured in nix

--- software/bspwm.nix
+++ software/bspwm.nix
@@ -4,7 +4,6 @@
 
   home.packages = with pkgs; [
     feh
-    dunst
     rofi
     betterlockscreen
     flameshot
@@ -69,13 +68,129 @@
 
     sxhkd = {
       enable = true;
-      extraConfig = builtins.readFile ../files/sxhkdrc;
+      keybindings = {
+        "super + Return" = "alacritty";
+        "super + d" = "rofi -show drun";
+        "super + Escape" = "pkill -USR1 -x sxhkd";
+        "alt + shift + x" = "betterlockscreen -l dimblur";
+        "super + {F9, F10, F11}" = "playerctl {previous,play-pause,next}";
+        "{XF86AudioPause,XF86AudioNext,XF86AudioPrev}" = "playerctl {play-pause,next,previous}";
+        "super + F1" = "firefox";
+        "super + F2" = "discord --no-sandbox";
+        "super + F3" = "exec spotify";
+        "super + F4" = "lutris";
+        "super + F5" = "XIVLauncher.Core";
+        "super + p" = "pavucontrol";
+        "super + u" = "exec bitwarden";
+        "super + e" = "emacs";
+        "super + v" = "emacsclient --eval \"(emacs-everywhere)\"";
+        "super + i" = "exec wacom.sh";
+        "super + o" = "exec change-source.sh";
+        "Print" = "flameshot gui";
+        "super + shift + Tab" = "exec rofi_powermenu";
+        "super + a" = "exec rofi-bluetooth";
+        "super + Tab" = "exec notify";
+        "super + space" = "xdotool key F19";
+        "super + alt + r" = "bspc wm -r";
+        "super + {_,shift + }q" = "bspc node -{c,k}";
+        "super + m" = "bspc desktop -l next";
+        "super + {t,shift + t,s,f}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+        "super + ctrl + {m,x,y,z}" = "bspc node -g {marked,locked,sticky,private}";
+        "super + {_,shift + }{Left,Down,Up,Right}" = "bspc node -{f,s} {west,south,north,east}";
+        "super + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+        "super + {_,shift + }c" = "bspc node -f {next,prev}.local";
+        "super + bracket{left,right}" = "bspc desktop -f {prev,next}.local";
+        "super + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+        "super + r" = "bspc node @/ -C forward";
+        "super + ctrl + {h,j,k,l}" = "bspc node -p {west,south,north,east}";
+        "super + ctrl + {1-9}" = "bspc node -o 0.{1-9}";
+        "super + ctrl + space" = "bspc node -p cancel";
+        "super + ctrl + shift + space" = "bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel";
+        "super + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+        "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
+        "super + alt + {Left,Down,Up,Right}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+        "super + alt + shift + {Left,Down,Up,Right}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
+        "super + ctrl + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
+      };
     };
 
     dunst = {
       enable = true;
       catppuccin.enable = false;
-      configFile = ../files/dunstrc;
+      settings = {
+        global = {
+          monitor = 0;
+          follow = "none";
+          width = 350;
+          height = 300;
+          origin = "top-right";
+          offset = "7x7";
+          scale = 0;
+          notification_limit = 0;
+
+          progress_bar = true;
+          progress_bar_height = 10;
+          progress_bar_frame_width = 1;
+          progress_bar_min_width = 150;
+          progress_bar_max_width = 300;
+
+          transparency = 30;
+          separator_height = 2;
+          padding = 8;
+          horizontal_padding = 8;
+          text_icon_padding = 0;
+          frame_width = 2;
+          frame_color = "#EA76CB";
+          separator_color = "frame";
+          corner_radius = 3;
+
+          font = "DokiDokiMono Nerd Font 14";
+          line_height = 0;
+          markup = "full";
+          alignment = "left";
+          vertical_alignment = "center";
+          show_age_threshold = 60;
+          ellipsize = "middle";
+          ignore_newline = "no";
+
+          format = "<b>%s</b>\n%b";
+
+          stack_duplicates = true;
+          hide_duplicate_count = false;
+          show_indicators = "yes";
+
+          sticky_history = "yes";
+          history_length = 20;
+
+          mouse_left_click = "close_current";
+          mouse_middle_click = "do_action, close_current";
+          mouse_right_click = "close_all";
+
+          always_run_script = true;
+          ignore_dbusclose = false;
+          force_xwayland = false;
+          force_xinerama = false;
+          indicate_hidden = "yes";
+          sort = "yes";
+        };
+
+        urgency_low = {
+          background = "#eff1f5";
+          foreground = "#4c4f69";
+          timeout = 6;
+        };
+        urgency_normal = {
+          background = "#eff1f5";
+          foreground = "#4c4f69";
+          timeout = 6;
+        };
+        urgency_critical = {
+          background = "#eff1f5";
+          foreground = "#4c4f69";
+          frame_color = "#d20f39";
+          timeout = 0;
+        };
+      };
     };
   };
 }
