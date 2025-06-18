144bfcf204580383e2217c98ef319e5877e225b4
pingu <nor@acorneroftheweb.com>
2025-06-18 09:13:57 +0200

    Tried kitty, "i" looked wrong

---
+++ software/kitty.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+let dokidokimono = import ./dokidokimono.nix { inherit pkgs; }; in
+
+{
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "DokiDokiMono Nerd Font";
+      package = dokidokimono;
+      size = 16;
+    };
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      window_padding_width = "5 3";
+      background_opacity = 0.93;
+    };
+  };
+}
