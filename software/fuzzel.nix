d6adc56194a5ddd64afff249c33bc4c42e2a6648
pingu <nor@acorneroftheweb.com>
2025-09-11 21:28:06 +0200

    Fuzzel is my new best friend

---
+++ software/fuzzel.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        font = "DokiDokiMono Nerd Font:size=18";
+        icons-enabled = false;
+        layer = "overlay";
+        vertical-pad = 10;
+        inner-pad = 10;
+      };
+      border = {
+        width = 2;
+        radius = 10;
+      };
+    };
+  };
+}
--- software/wofi.nix
+++ software/wofi.nix
@@ -4,79 +4,79 @@
   programs.wofi = {
     enable = true;
     settings = {
-      width=500;
-      height=300;
-      always_parse_args=true;
-      show_all=true;
-      print_command=true;
-      layer="overlay";
-      insensitive=true;
-      content_halign="center";
-      prompt="";
+      width = 500;
+      height = 300;
+      always_parse_args = true;
+      show_all = true;
+      print_command = true;
+      layer = "overlay";
+      insensitive = true;
+      content_halign = "center";
+      prompt = "";
     };
     style = ''
-    window {
-      margin: 0px;
-      border: 2px solid #eff1f5;
-      border-radius: 0px;
-      background-color: #eff1f5;
-      font-family: DokiDokiMono Nerd Font;
-      font-size: 18px;
-    }
+      window {
+        margin: 0px;
+        border: 2px solid #eff1f5;
+        border-radius: 0px;
+        background-color: #eff1f5;
+        font-family: DokiDokiMono Nerd Font;
+        font-size: 18px;
+      }
 
-    #input {
-      margin: 15px;
-      border: 1px solid #eff1f5;
-      border-radius: 15px;
-      color: #4c4f69;
-      background-color: #eff1f5;
-    }
+      #input {
+        margin: 15px;
+        border: 1px solid #eff1f5;
+        border-radius: 15px;
+        color: #4c4f69;
+        background-color: #eff1f5;
+      }
 
-    #input image {
-      color: #4c4f69;
-    }
+      #input image {
+        color: #4c4f69;
+      }
 
-    #inner-box {
-      margin: 5px;
-      border: none;
-      padding-left: 10px;
-      padding-right: 10px;
-      background-color: #eff1f5;
-    }
+      #inner-box {
+        margin: 5px;
+        border: none;
+        padding-left: 10px;
+        padding-right: 10px;
+        background-color: #eff1f5;
+      }
 
-    #outer-box {
-      margin: 5px;
-      border: none;
-      background-color: #eff1f5;
-    }
+      #outer-box {
+        margin: 5px;
+        border: none;
+        background-color: #eff1f5;
+      }
 
-    #scroll {
-      margin: 0px;
-      border: none;
-    }
+      #scroll {
+        margin: 0px;
+        border: none;
+      }
 
-    #text {
-      margin: 5px;
-      border: none;
-      color: #4c4f69;
-    }
+      #text {
+        margin: 5px;
+        border: none;
+        color: #4c4f69;
+      }
 
-    #entry {
-      padding-left: 20px;
-      padding-right: 20px;
-    }
+      #entry {
+        padding-left: 20px;
+        padding-right: 20px;
+      }
 
-    #entry:selected {
-      background-color: #ea76cb;
-      font-weight: normal;
-      border-radius: 15px;
-    }
+      #entry:selected {
+        background-color: #ea76cb;
+        font-weight: normal;
+        border-radius: 15px;
+      }
 
-    #text:selected {
-      background-color: #ea76cb;
-      color: #4c4f69;
-      font-weight: normal;
-    }
+      #text:selected {
+        background-color: #ea76cb;
+        color: #4c4f69;
+        font-weight: normal;
+      }
     '';
   };
 }
