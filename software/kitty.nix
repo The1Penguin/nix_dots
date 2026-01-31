ea562d73542891dd6c127962e537c23eed5fd7f4
pingu <nor@acorneroftheweb.com>
2026-01-31 21:14:09 +0100

    Kitty looks a bit better

--- software/kitty.nix
+++ software/kitty.nix
@@ -6,7 +6,7 @@ let dokidokimono = import ./dokidokimono.nix { inherit pkgs; }; in
   programs.kitty = {
     enable = true;
     font = {
-      name = "family=\"DokiDokiMono Nerd Font\"";
+      name = "family=\"DokiDokiMono Nerd Font\" wght=380";
       package = dokidokimono;
       size = 16;
     };
@@ -18,11 +18,10 @@ let dokidokimono = import ./dokidokimono.nix { inherit pkgs; }; in
       window_padding_width = "2 3";
       background_opacity = 0.93;
       font_features = "none";
+      text_composition_strategy = "1.3 15";
+      "modify_font baseline" = -2;
+      "modify_font cell_width" = -1;
+      "modify_font cell_height" = 0.5;
     };
-    extraConfig = ''
-      modify_font baseline -2
-      modify_font cell_width -1
-      modify_font cell_height 0.5
-    '';
   };
 }
